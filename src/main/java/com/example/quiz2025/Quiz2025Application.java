6a74fdc1aa44be0434ea83b7514d6ac6c5e8e28b
suhyeon <shbyun9808@gmail.com>
2025-11-05 15:39:03 +0900

    back

---
+++ src/main/java/com/example/quiz2025/Quiz2025Application.java
@@ -0,0 +1,13 @@
+package com.example.quiz2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz2025Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz2025Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/quiz2025/Quiz2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quiz2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Quiz2025ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
