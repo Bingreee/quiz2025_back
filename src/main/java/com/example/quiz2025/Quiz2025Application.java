c07ff802ddcaab939053303d42d6b737cda9a142
suhyeon <shbyun9808@gmail.com>
2025-11-20 15:07:54 +0900

    feat(openai): open ai 사용 설정

--- src/main/java/com/example/quiz2025/Quiz2025Application.java
+++ src/main/java/com/example/quiz2025/Quiz2025Application.java
@@ -1,11 +1,14 @@
 package com.example.quiz2025;
 
+import com.example.quiz2025.quiz2025src.config.GptConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(GptConfig.class)
 public class Quiz2025Application {
 
 	public static void main(String[] args) {
